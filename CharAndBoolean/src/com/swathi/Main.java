4b093e0bd183572fadbc612628a06438332a19ae
Your Name <you@example.com>
2020-11-16 18:36:30 -0800

    add charAndBoolean folder

---
+++ CharAndBoolean/src/com/swathi/Main.java
@@ -0,0 +1,11 @@
+package com.swathi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	char myChar = '\u00A9'; //copyright sign
+        System.out.println("unicode value was "+myChar);
+    boolean myBoolean = true;
+    boolean myOtherBoolean = false;
+    }
+}
