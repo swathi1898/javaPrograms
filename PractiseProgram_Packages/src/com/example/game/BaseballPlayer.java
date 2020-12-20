b96c8352292abef64ddf9464c2261e8a6f355c9d
Your Name <swathis1898@gmail.com>
2020-12-19 16:42:15 -0800

    deleted PractiseProgram_Packages folder

--- PractiseProgram_Packages/src/com/example/game/BaseballPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.example.game;
-
-public class BaseballPlayer extends Player {
-    public BaseballPlayer(String name) {
-        super(name);
-    }
-}
--- PractiseProgram_Packages/src/com/example/game/FootballPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.example.game;
-
-public class FootballPlayer extends Player {
-    public FootballPlayer(String name) {
-        super(name);
-    }
-}
