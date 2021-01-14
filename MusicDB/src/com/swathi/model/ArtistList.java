f711dc585aa363aaf9db64d0919d397c7df43851
swathi <swathis1898@gmail.com>
2021-01-15 00:29:00 +0530

    add MusicDB, MusicUI, EchoServer and EchoClient files

---
+++ MusicDB/src/com/swathi/model/ArtistList.java
@@ -0,0 +1,40 @@
+package com.swathi.model;
+
+public class ArtistList {
+    private String artistName;
+    private String albumName;
+    private int track;
+    private String title;
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public int getTrack() {
+        return track;
+    }
+
+    public void setTrack(int track) {
+        this.track = track;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
