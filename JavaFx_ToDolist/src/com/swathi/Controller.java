19c2b7eb4f5d1fdfd8a4ab7387e0434f019757e7
Your Name <swathis1898@gmail.com>
2021-02-15 07:53:04 -0800

    added files in javaFx

--- JavaFx_ToDolist/src/com/swathi/Controller.java
+++ JavaFx_ToDolist/src/com/swathi/Controller.java
@@ -5,11 +5,12 @@ import com.swathi.datamodel.TodoItem;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextArea;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
@@ -25,6 +26,8 @@ public class Controller {
     private TextArea itemDetailsTextArea;
     @FXML
     private Label deadLineLabel;
+    @FXML
+    private BorderPane mainBorderPane;
 
     public void initialize(){
 //        TodoItem item1 =  new TodoItem("Cleaning room","routine clean work and change window curtains",
@@ -60,6 +63,20 @@ public class Controller {
        todoListView.getSelectionModel().selectFirst();
 
 
+    }
+
+    @FXML
+    public void showNewItemDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("todoItemDialog.fxml"));
+            dialog.getDialogPane().setContent(root);
+        }catch (IOException e){
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
     }
 
     @FXML
